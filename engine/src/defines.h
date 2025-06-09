052da2388f4b3ab45359c239641076da065ce8cf
lortkipa <nikusha191208@gmail.com>
2025-06-09 22:03:11 +0400

    defines done

---
+++ engine/src/defines.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdint.h>
+
+/*--------------------booleans--------------------*/
+typedef uint8_t b8;
+#define true (1)
+#define false (0)
+#define null (void*)0
+
+/*--------------------floats--------------------*/
+typedef float f32;
+typedef double f64;
+
+/*--------------------signed intigers--------------------*/
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef int_fast8_t fi8;
+typedef int_fast16_t fi16;
+typedef int_fast32_t fi32;
+typedef int_fast64_t fi64;
+
+/*--------------------unsigned intigers--------------------*/
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef uint_fast8_t fu8;
+typedef uint_fast16_t fu16;
+typedef uint_fast32_t fu32;
+typedef uint_fast64_t fu64;
+
+/*--------------------platform--------------------*/
+#if defined(__linux__)
+    #define PLATFORM_LINUX
+#elif defined(_WIN32)
+    #define PLATFORM_WINDOWS
+#else
+    #error Platform Running On Unsuported Platform!
+#endif
--- engine/src/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void Test(void)
-{
-    printf("Hello World\n");
-}
